3e43c4711f8f192c1a36b49a3874f28ef774130a
Yu3dan <144178995+Yu3dan@users.noreply.github.com>
2023-10-11 12:10:45 -0500

    Added URL Note to Module 1

--- CS325_p3/module_1/reddit_downloader.py
+++ CS325_p3/module_1/reddit_downloader.py
@@ -3,6 +3,7 @@
 # CS325-02 Project 3 Module 1
 # Purpose: The purpose of this module in Project 3 is to take a URL given by the user and download the HTML (source code) then write it into another file named "html.txt" and store it into folder Data and subfolder raw.
 # Using the sys module it allows the program to take an argument given by a user. (In this case a Reddit URL). Then it uses urllib.request to access the URL's source code and download it into the directory in quotes.
+# NOTE: In order for this to work, you must still use an OLD reddit URL similarly to project 1 and 2. Ex: " https://old.reddit.com/r/OnePiece/comments/174xxx0/i_get_its_an_purely_optical_thing_but/"
 
 import sys
 import urllib.request
