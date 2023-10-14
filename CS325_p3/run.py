1a07a3278a001eb2625fcee89bcc140f8e932019
Yu3dan <144178995+Yu3dan@users.noreply.github.com>
2023-10-14 12:32:39 -0500

    Update run.py

--- CS325_p3/run.py
+++ CS325_p3/run.py
@@ -1,3 +1,5 @@
+#This python file adds all modules together and works as glue to make sure each function is carried over to the next step. Thus doing tthe whole process in one go and writing out all files to the correct location.
+
 from module_1 import reddit_downloader
 from module_2 import parsing_html
 from module_3 import removing_html
